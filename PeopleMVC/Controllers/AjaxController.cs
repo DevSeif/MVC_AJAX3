6b3ca135cf2f230e8f73017e164a5c04ab987659
DevSeif <65452750+DevSeif@users.noreply.github.com>
2022-11-10 14:02:39 +0100

    Add files via upload

---
+++ PeopleMVC/Controllers/AjaxController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleMVC.Models;
+
+
+
+namespace PeopleMVC.Controllers
+{
+    public class AjaxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(PeopleViewModel.people);
+        }
+
+        [HttpGet]
+        public IActionResult FetchPeople()
+        {
+            PeopleViewModel.FetchPeople();
+            return PartialView("_PeoplePartial", PeopleViewModel.people);
+        }
+
+        [HttpPost]
+        public IActionResult ShowDetails(string id)
+        {
+            foreach (Person person in PeopleViewModel.people)
+            {
+                if (person.Id == id.Trim())
+                {
+                    //var json = JsonConvert.SerializeObject(person);
+                    return PartialView("_PersonPartial", person);
+                }
+            }
+            return PartialView("_PeoplePartial", PeopleViewModel.people);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string id)
+        {
+            foreach (Person person in PeopleViewModel.people)
+            {
+                if (person.Id == id.Trim())
+                {
+                    PeopleViewModel.people.Remove(person);
+                    return Json(id + " was deleted");
+                }
+
+            }
+
+            return Json("Error could not delete");
+        }
+
+        [HttpGet]
+        public IActionResult Refresh()
+        {
+            return PartialView("_PeoplePartial", PeopleViewModel.people);
+        }
+
+    }
+}
